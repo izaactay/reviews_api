080b316c6578cb79ea52fb600362a7b9f0e1accf
Izaac Tay <izaactay1@gmail.com>
2023-05-17 13:23:27 -0700

    fixed place_id bug

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from review_functions import create_url, get_reviews
 
 
 
-place_id = "0x60188cf2ebd0b05b:0x95294f4eb1559d4e"
+#place_id = "0x60188cf2ebd0b05b:0x95294f4eb1559d4e"
 
 app = FastAPI()
 
 @app.get('/{p_id}')
 def get_review_score(p_id: str):
     next_page_token = ""
-    url = create_url(place_id, next_page_token)
+    url = create_url(p_id, next_page_token)
     token, total_reviews, local_reviews = get_reviews(url, 1)
     count = 1
     while token != "" and count < 10:
-        url = create_url(place_id, token)
+        url = create_url(p_id, token)
         token, temp_total, temp_local = get_reviews(url, 2)
         total_reviews += temp_total
         local_reviews += temp_local

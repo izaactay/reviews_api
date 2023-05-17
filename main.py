bf953241e5c1119c72808d101c64a04d921e9add
Izaac Tay <izaactay1@gmail.com>
2023-05-17 01:46:18 -0700

    Added main.py

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-
-import
-
-def print_hi(name):
-
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from fastapi import FastAPI
+from review_functions import create_url, get_reviews
 
 
 
-if __name__ == '__main__':
-    print_hi('world')
+
+
+place_id = "0x60188cf2ebd0b05b:0x95294f4eb1559d4e"
+
+app = FastAPI()
+
+@app.get('/{p_id}')
+def get_review_score(p_id: str):
+    next_page_token = ""
+    url = create_url(place_id, next_page_token)
+    token, total_reviews, local_reviews = get_reviews(url, 1)
+    count = 1
+    while token != "" and count < 10:
+        url = create_url(place_id, token)
+        token, temp_total, temp_local = get_reviews(url, 2)
+        total_reviews += temp_total
+        local_reviews += temp_local
+        count += 1
+    return (local_reviews/total_reviews)*100
+
+
 
 
---
+++ review_functions.py
@@ -0,0 +1,52 @@
+import requests
+import bs4
+
+def create_url(place_id,token=""):
+    url = "https://www.google.com/async/reviewDialog?hl=en_us&async=feature_id:" + place_id + ",next_page_token:" + token + ",sort_by:qualityScore,start_index:,associated_topic:,_fmt:pc"
+    return url
+
+
+def get_reviews(url,page):
+
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content,'html.parser')
+    user = []
+    location_info = {}
+    data_id = ''
+    token = ''
+    total_reviews = 0
+    local_reviews = 0
+    if page == 1:
+        for el in soup.select('.lcorif'):
+            data_id = soup.select_one('.loris')['data-fid']
+            token = soup.select_one('.gws-localreviews__general-reviews-block')['data-next-page-token']
+            location_info = {
+               'title': soup.select_one('.P5Bobd').text.strip(),
+               'address': soup.select_one('.T6pBCe').text.strip(),
+               'avgRating': soup.select_one('span.Aq14fc').text.strip(),
+               'totalReviews': soup.select_one('span.z5jxId').text.strip()
+            }
+    else:
+        for el in soup.select('.lcorif'):
+            data_id = soup.select_one('.loris')['data-fid']
+            token = soup.select_one('.gws-localreviews__general-reviews-block')['data-next-page-token']
+
+    for el in soup.select('.gws-localreviews__google-review'):
+        total_reviews += 1
+        temp_dict = {'name': el.select_one('.TSUbDb').text.strip(),
+                     'link': el.select_one('.TSUbDb a')['href'],
+                     'rating': el.select_one('.BgXiYe .lTi8oc')['aria-label'],
+                     'review': el.select_one('.Jtu6Td').text.strip()}
+        full_review_text = el.select_one('.review-full-text')
+        if full_review_text == None:
+            temp_dict['review'] = el.select_one('.Jtu6Td').text.strip()
+            if temp_dict['review'] == '':
+                total_reviews-=1
+        else:
+            temp_dict['review'] = full_review_text.text.strip()
+            local_reviews += 1
+
+
+        user.append(temp_dict)
+
+    return token,total_reviews,local_reviews
